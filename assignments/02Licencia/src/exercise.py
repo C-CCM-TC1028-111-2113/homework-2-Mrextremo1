06d5be64c28e96e8a0dc4d67e2a06ea48a52865f
Leonardo <leonardomedrano23@hotmail.com>
2021-08-22 02:18:19 -0500

    done

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,7 +1,22 @@
 
 def main():
     #Escribe tu código debajo de esta línea
-    pass
+    edad = int(input("Ingresa tu edad: "))
+    id = str(input("¿Tienes identificación oficial? (s/n): "))
+
+    if edad >= 18:
+        if id == str("s"):
+            x = "Trámite de licencia concedido"
+        elif id == str("n"):
+            x = "No cumples requisitos"
+        else:
+            x = "Respuesta incorrecta"
+    elif edad < 18 and edad > 0:
+        x = "No cumples requisitos"
+    else:
+        x = "Respuesta incorrecta"
+    
+    print(x)
 
 
 if __name__ == '__main__':
--- assignments/05Ordena/src/exercise.py
+++ assignments/05Ordena/src/exercise.py
@@ -1,6 +1,46 @@
 def main():
     # Escribe el código adecuado para completar el programa
-    pass
+    num1 = int(input("Ingresa el primer número: "))
+    num2 = int(input("Ingresa el segundo número: "))
+    num3 = int(input("Ingresa el tercer número: "))
+    x = int()
+    y = int()
+    z = int()
+
+    if num1 < num2:
+        if num3 < num2:
+            if num1 < num3:
+                x = num2
+                y = num3
+                z = num1
+            elif num1 > num3:
+                x = num2
+                y = num1
+                z = num3
+        
+            
+    if num3 > num1:
+        if num3 > num2:
+            if num2 > num1:
+                x = num3
+                y = num2
+                z = num1
+            elif num1 > num2:
+                x = num3
+                y = num1
+                z = num2
+    elif num3 < num2:
+        x = num1
+        y = num2
+        z = num3
+    else:
+        x = num1
+        y = num3
+        z = num2
+    
+    print(x)
+    print(y)
+    print(z)
 
 
 if __name__=='__main__':
